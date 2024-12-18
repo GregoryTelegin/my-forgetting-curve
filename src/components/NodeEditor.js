ffee3459418d78954bd7cb0ce21c3420c2876f07
Gregory <120729699+GregoryTelegin@users.noreply.github.com>
2024-12-19 03:59:29 +0500

    Delete src/components/NodeEditor.js

--- src/components/NodeEditor.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Input } from 'antd';
-const NodeEditor = ({ value, onChange, onSave }) => {
-    return (_jsx(Input, { value: value, onChange: (e) => onChange(e.target.value), onBlur: onSave, onPressEnter: onSave, autoFocus: true, className: "editable-input" }));
-};
-export default NodeEditor;
